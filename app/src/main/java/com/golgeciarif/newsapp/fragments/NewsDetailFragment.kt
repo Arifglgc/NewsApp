8f99387e8d5405e0fb8a8e5d539636c3afe85287
Arif Golgeci <69479319+Arifglgc@users.noreply.github.com>
2024-04-23 23:06:07 +0300

    Implement binding features in NewsDetailFragment

--- app/src/main/java/com/golgeciarif/newsapp/fragments/NewsDetailFragment.kt
+++ app/src/main/java/com/golgeciarif/newsapp/fragments/NewsDetailFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.golgeciarif.newsapp.databinding.NewsDetailsBinding
+import com.golgeciarif.newsapp.newsPojo.Article
 
 class NewsDetailFragment: Fragment() {
     private lateinit var binding: NewsDetailsBinding
@@ -23,6 +25,19 @@ class NewsDetailFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var article: Article? = arguments?.getParcelable("new")
+
+        if (article!= null)
+        {
+            binding.tvNewsTitle.text = article.title
+            binding.tvNewsAuthor.text=article.author
+            binding.tvNewsDate.text=article.publishedAt
+            binding.tvContent.text=article.content
+
+            Glide.with(requireActivity()).load(article.urlToImage).into(binding.ivNews)
+        }
+
     }
 
 }
